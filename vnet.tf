9438b2b63ce0b16f6720f93aadd69a198fb74d2a
sshachidixit <117880753+sshachidixit@users.noreply.github.com>
2023-09-26 16:39:42 +0530

    Adding vnet.tf and vnet input variable file

--- vnet.tf
+++ vnet.tf
@@ -4,3 +4,13 @@
     location = "South India"
   
 }*/
+
+#variablize the vnet creation
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "${local.resource_name_prefix}-${var.vnet_name}"
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.comman_tags
+
+}
---
+++ vnet_input_vars.tf
@@ -0,0 +1,70 @@
+#virtual network name
+variable "vnet_name" {
+  description = "Virtual network name"
+  type        = string
+  default     = "default_vnet"
+
+}
+
+#virtual network address space
+variable "vnet_address_space" {
+  description = "vnet address space or cidr block"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+
+}
+
+# Web Subnet Name
+variable "web_subnet_name" {
+  description = "Virtual Network Web Subnet Name"
+  type        = string
+  default     = "websubnet"
+}
+# Web Subnet Address Space
+variable "web_subnet_address" {
+  description = "Virtual Network Web Subnet Address Spaces"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+
+# App Subnet Name
+variable "app_subnet_name" {
+  description = "Virtual Network App Subnet Name"
+  type        = string
+  default     = "appsubnet"
+}
+# App Subnet Address Space
+variable "app_subnet_address" {
+  description = "Virtual Network App Subnet Address Spaces"
+  type        = list(string)
+  default     = ["10.0.11.0/24"]
+}
+
+
+# Database Subnet Name
+variable "db_subnet_name" {
+  description = "Virtual Network Database Subnet Name"
+  type        = string
+  default     = "dbsubnet"
+}
+# Database Subnet Address Space
+variable "db_subnet_address" {
+  description = "Virtual Network Database Subnet Address Spaces"
+  type        = list(string)
+  default     = ["10.0.21.0/24"]
+}
+
+
+# Bastion / Management Subnet Name
+variable "bastion_subnet_name" {
+  description = "Virtual Network Bastion Subnet Name"
+  type        = string
+  default     = "bastionsubnet"
+}
+# Bastion / Management Subnet Address Space
+variable "bastion_subnet_address" {
+  description = "Virtual Network Bastion Subnet Address Spaces"
+  type        = list(string)
+  default     = ["10.0.100.0/24"]
+}
